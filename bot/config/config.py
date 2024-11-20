2f906e48973709bbff274fe3a22f57f948cdd5a5
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-21 03:56:41 +0700

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -4,26 +4,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
-    API_ID: int = 0
-    API_HASH: str = ""
+    API_ID: int
+    API_HASH: str
 
     REF_LINK: str = ""
     AUTO_TASK: bool = True
     AUTO_CONNECT_WALLET: bool = False
-
-    AUTO_TASK: bool = True
-    AUTO_CONNECT_WALLET: bool = False
-
     DELAY_EACH_ACCOUNT: list[int] = [20, 30]
     IGNORE_TASKS: list[str] = ["boost"]
     ADVANCED_ANTI_DETECTION: bool = True
-
     USE_PROXY_FROM_FILE: bool = False
 
-
-settings = Settings()
-
 if not os.path.exists(".env"):
-    print("Warning: .env file not found. Default values will be used.")
+    print("Warning: .env file not found. Default values may be used.")
 
 settings = Settings()
