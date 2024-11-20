8cb2bb5606f949e9db64843a7e16b29224c716e3
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-21 03:33:06 +0700

    Update registrator.py

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,5 +1,4 @@
 from pyrogram import Client
-
 from bot.config import settings
 from bot.utils import logger
 
@@ -9,12 +8,17 @@ async def register_sessions() -> None:
     API_HASH = settings.API_HASH
 
     if not API_ID or not API_HASH:
-        raise ValueError("API_ID and API_HASH not found in the .env file.")
+        raise ValueError("API_ID and API_HASH are not found in the .env file. Please check the configuration.")
 
-    session_name = input('\nEnter the session name (press Enter to exit): ')
+    session_name = input('\nEnter the session name (press Enter to exit): ').strip()
 
     if not session_name:
-        return None
+        print("Session registration process canceled.")
+        return
+
+    if not session_name.isalnum():
+        print("Session name can only contain letters and numbers.")
+        return
 
     session = Client(
         name=session_name,
@@ -23,7 +27,9 @@ async def register_sessions() -> None:
         workdir="sessions/"
     )
 
-    async with session:
-        user_data = await session.get_me()
-
-    logger.success(f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}')
+    try:
+        async with session:
+            user_data = await session.get_me()
+        logger.success(f"Session successfully added @{user_data.username} | {user_data.first_name} {user_data.last_name}")
+    except Exception as e:
+        logger.error(f"Failed to add session: {e}")
