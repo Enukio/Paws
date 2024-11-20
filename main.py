abb4058e2fece7588c78cbac1956b31fa436e9d5
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-21 03:52:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import asyncio
+import logging
 from contextlib import suppress
 
 from bot.utils.launcher import process
+from bot.config.config import settings
 
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+def validate_settings():
+    
+    if not settings.API_ID or not settings.API_HASH:
+        logger.warning("API_ID and API_HASH are not properly set. Please check your .env file.")
+    else:
+        logger.info("API_ID and API_HASH are properly configured.")
 
 async def main():
+    validate_settings()
     await process()
-
 
 if __name__ == '__main__':
     with suppress(KeyboardInterrupt):
