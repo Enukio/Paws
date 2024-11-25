0f8371fc140d990ab24c0e8ce9c4ce744960b503
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-25 15:43:12 +0700

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 logger.remove()
 logger.add(sink=sys.stdout, format="<r>[Paws]</r> | <white>{time:YYYY-MM-DD HH:mm:ss}</white>"
-                                   " | <level>{level: <8}</level>"
-                                   " | <cyan><b>{line}</b></cyan>"
-                                   " - <white><b>{message}</b></white>")
+                                   " | <level>{level: <7}</level>"
+                                   " | <cyan><b>{line <3}</b></cyan>"
+                                   " | <white><b>{message}</b></white>")
 logger = logger.opt(colors=True)
